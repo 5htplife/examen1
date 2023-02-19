9412612ba6947e12d63408a44914b5e23227847b
5htplife <ilinevich@nes.ru>
2023-02-18 21:26:03 -0600

    new update

--- main.py
+++ main.py
@@ -137,6 +137,7 @@ with st.echo(code_location="below"):
        'Total omega-6 fat', 'Monounsaturated fatty acids', 'Saturated fat',
        'Total protein', 'Total carbohydrates', 'iso3']]
     nutrition_macro = nutrition_macro.merge(iso, how='inner', left_on = 'iso3', right_on = 'iso3c')
+    nutrition_macro
     countries_2 = nutrition_macro['country_name'].astype(str)
     country_options_2 = st.selectbox('Choose a country', countries_2)
     per_country_habits = nutrition_macro[nutrition_macro['country_name'] == country_options]
