d4deb5c46256f2a36249dcd2b5644885439a4dae
5htplife <ilinevich@nes.ru>
2023-02-28 17:16:24 -0600

    new update

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ with st.echo(code_location="below"):
     country_options = st.selectbox('Choose a country', countries)
     per_country_habits = nutrition_percent[nutrition_percent['country_name'] == country_options]
     country1 = per_country_habits.drop(
-        columns=['Unnamed: 0_x', 'Unnamed: 0_y', 'Unnamed: 0.1', 'iso3', 'age', 'female', 'urban', 'edu',
+        columns=['Unnamed: 0_x', 'Unnamed: 0_y', 'iso3', 'age', 'female', 'urban', 'edu',
                  'year', 'Vitamin B9', 'Vitamin B3', 'Vitamin B2', 'Zinc',
                  'Vitamin E', 'Vitamin D', 'Vitamin C', 'Vitamin B12', 'Vitamin B6',
                  'Vitamin A', 'Selenium', 'Potassium', 'Magnesium', 'Iron', 'Iodine',
